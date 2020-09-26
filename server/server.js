afce406a29189b82d40873926b477f2041ec2cb0
mosesbenjamin <mavewreek@gmail.com>
2020-09-26 19:20:02 +0100

    Setting up mongoose

---
+++ server/server.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+   text: {
+        type: String
+   },
+   completed: {
+        type: Boolean
+   },
+   completedAt: {
+        type: Number
+   } 
+});
+
+// var newTodo = new Todo({
+//     text: "Cook dinner"
+// });
+
+// newTodo.save().then((doc)=>{
+//     console.log('Saved todo', doc)
+// }, (e)=>{
+//     console.log('Unable to save todo', e)
+// })
+
+var myTodo = new Todo({
+    text: 'Successfully set up mongoose',
+    completed: true,
+    completedAt: 7
+});
+
+myTodo.save().then((doc)=>{
+    console.log('Saved todo', doc);
+}, (e)=>{
+    console.log('Unable to save todo', e);
+});
+
